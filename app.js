4ff161f45e345730652415f25f66e7db788f0de5
N1flight <nadersalem123@gmail.com>
2021-04-07 15:41:27 -0500

    added questions and functions teammembers and continueprompt-

--- app.js
+++ app.js
@@ -33,3 +33,109 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+let newEmployee;
+let allEmployees = [];
+let currentEmployeeQuestion; 
+let currentEmployee;
+
+const managerQuestion = [
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please enter your office number.',
+    }
+]
+const engineerQuestion = [
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'Please enter your GitHub username.',
+    },
+]
+const internQuestion = [
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter the name of your school.',
+    },
+]
+function teamMembers(){
+    inquirer    
+        .prompt([
+            {
+                type: 'list',
+                name: 'position',
+                message: 'Please select your position with the company',
+                choices: ['Engineer', 'Intern', 'Manager', 'Quit']
+            },
+        ]).then((response) => {
+            if (response.position === 'Quit') {
+                try {
+                    fs.writeFileSync(outputPath, render(allEmployees))
+                    console.log('Congrats you made a team!');
+                    return;
+                } catch(err) {
+                    console.error(err);
+                }
+            }
+            else {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is your name?.',
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'Please enter your ID number.',
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Please enter your email address.',
+                    },
+                ]).then(response2 => {
+                    currentEmployee = {
+                        ...response,
+                        ...response2
+                    }
+                    switch (response.position){
+                        case 'Engineer': 
+                            currentEmployeeQuestion = engineerQuestion;
+                            continuePrompt();
+                            break;
+                        case 'Intern':
+                            currentEmployeeQuestion = internQuestion;
+                            continuePrompt();
+                            break;
+                        case 'Manager':
+                            currentEmployeeQuestion = managerQuestion;
+                            continuePrompt();
+                            break;
+                    }
+                })
+            }
+        })
+}
+function continuePrompt() {
+    inquirer    
+    .prompt(currentEmployeeQuestion).then((response) => {
+        switch (currentEmployee.position){
+            case 'Engineer': 
+                newEmployee =  new Engineer(currentEmployee.name, currentEmployee.id, currentEmployee.email, response.gitHub);                
+                break;
+            case 'Intern':
+                newEmployee = new Intern(currentEmployee.name, currentEmployee.id, currentEmployee.email, response.school)              
+                break;
+            case 'Manager':
+                newEmployee = new Manager(currentEmployee.name, currentEmployee.id, currentEmployee.email, response.officeNumber)                    
+                break;
+        }
+        console.log(newEmployee)
+        allEmployees.push(newEmployee);
+        console.log(allEmployees);
+    }).then( () => teamMembers());
+}
+teamMembers();
